fcbead952f022018c5fedf74a303c719d7953f4e
EzAmmar99 <41229543+EzAmmar99@users.noreply.github.com>
2019-06-29 22:01:34 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -38,4 +38,92 @@ const siteContent = {
 };
 
 // write your code here 
+const container = document.querySelector('.container')
+const navItems = container.querySelectorAll('a')
+
+navItems.forEach((element, index) => {
+  element.textContent = siteContent.nav[`nav-item-${index + 1}`]
+})
+
+const navImg = container.querySelector('.logo')
+navImg['src']  =  siteContent.nav['img-src']
+
+const cta = container.querySelector('.cta')
+const cta_text = cta.querySelector('.cta-text')
+
+const h1 = cta_text.querySelector('h1')
+h1.textContent = siteContent.cta['h1']
+
+const button = cta_text.querySelector('button')
+button.textContent = siteContent.cta['button']
+
+const img = cta.querySelector('img')
+img['src'] = siteContent.cta['img-src']
+
+const main_content = container.querySelector('.main-content')  
+const top_content = main_content.querySelector('.top-content')  
+const text_content = top_content.querySelectorAll('.text-content') 
+
+const features = text_content[0] ;
+const features_h4 = features.querySelector('h4')
+features_h4.textContent = siteContent['main-content']['features-h4']
+
+const features_content = features.querySelector('p')
+features_content.textContent = siteContent['main-content']['features-content']
+
+const about = text_content[1] ;
+const about_h4 = about.querySelector('h4')
+about_h4.textContent = siteContent['main-content']['about-h4']
+
+const about_content = about.querySelector('p')
+about_content.textContent = siteContent['main-content']['about-content']
+
+const middle_img_src = main_content.querySelector('img')
+middle_img_src['src'] = siteContent['main-content']['middle-img-src']
+
+const bottom_content = container.querySelector('.bottom-content')
+const text_content_services = bottom_content.querySelectorAll('.text-content')
+
+const services = text_content_services[0]
+const services_h4 = services.querySelector('h4')
+services_h4.textContent = siteContent['main-content']['services-h4']
+
+const services_content = services.querySelector('p')
+services_content.textContent = siteContent['main-content']['services-content']
+
+const product = text_content_services[1]
+const product_h4 = product.querySelector('h4')
+product_h4.textContent = siteContent['main-content']['product-h4']
+
+const product_content = product.querySelector('p')
+product_content.textContent = siteContent['main-content']['product-content']
+
+const vision = text_content_services[2]
+const vision_h4 = vision.querySelector('h4')
+vision_h4.textContent = siteContent['main-content']['vision-h4']
+
+const vision_content = vision.querySelector('p')
+vision_content.textContent = siteContent['main-content']['vision-content']
+
+const contact = container.querySelector('.contact')
+const contact_h4 = contact.querySelector('h4')
+contact_h4.textContent = siteContent['contact']['contact-h4']
+
+const contacts = contact.querySelectorAll('p')
+const address = contacts[0]
+address.textContent = siteContent['contact']['address']
+
+const phone = contacts[1]
+phone.textContent = siteContent['contact']['phone']
+
+const email = contacts[2]
+email.textContent = siteContent['contact']['email']
+
+const footer = container.querySelectorAll('footer')
+footer[0].textContent = siteContent['footer']['copyright']
+
+
+button.addEventListener('click', (e)=>{
+  location.reload();
+})
 
